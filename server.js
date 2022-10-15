ecc40bd32af3b5365bbaaafb8d5cc0b8a035fbb1
srose74 <srose74@icloud.com>
2022-10-15 13:23:26 +1100

    Setup Take 2

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3001; // Note: using a different port to normal
+
+app.use(express.json());
+app.use(express.static("./client/build"));
+
+app.get("/api/test", (req, res) => {
+  res.json({ result: "success" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
